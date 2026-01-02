68d16f1fecfa3e47ab81be164af7ddc2c094fc00
Ambesh Yadav <168898559+HELLOWORLDAMBESH@users.noreply.github.com>
2026-01-02 21:03:51 +0530

    Add files via upload

---
+++ hellosd.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+    double r =10;
+    double area = 3.14159 * r * r;
+    std::cout << "Area of circle with radius " << r << " is " << area << std::endl;
+
+    return 0;
+}
+
+
